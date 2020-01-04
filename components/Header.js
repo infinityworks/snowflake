1b94373e262e36ed148731e7e23432b43e1ff959
Ivor Caldwell <ivor.caldwell@infinityworks.com>
2020-01-04 09:23:17 +0000

    PDF wip

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,76 @@
 import React from 'react'
+import { tracks } from '../constants'
+import type { MilestoneMap, Milestone } from '../constants'
+import pdfMake, { format } from "pdfmake/build/pdfmake";
+import pdfFonts from "pdfmake/build/vfs_fonts";
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 export default class Header extends React.Component {
+
     generatePdf() {
-        alert("Download!");
+        var body = [
+            [], // Header
+            [], // Description
+        ];
+        const HEADER = 0;
+        const DESCRIPTION = 1;
+        const LEVEL_BASE = 2;
+        const splitMultiline = s => s.replace(/<br\/>/g, '\n');
+        for (const [key, value] of Object.entries(tracks)) {
+            body[HEADER].push({
+                text: value.displayName,
+                fontSize: 10,
+                bold: true
+            });
+            body[DESCRIPTION].push(splitMultiline(value.description));
+            value.milestones.forEach((level, index) => {
+                const levelIndex = LEVEL_BASE + index;
+                console.log(`index=${index}, levelIndex=${levelIndex}, body.length=${body.length}`);
+                if (body.length <= levelIndex) {
+                    console.log('Adding row');
+                    body.push([]);
+                }
+                body[levelIndex].push(
+                    [
+                        { text: `Level ${index + 1} Core`, fontSize: 10, color: '#666666' },
+                        ' ',
+                        splitMultiline(level.signals),
+                        ' ',
+                        { text: 'Examples', color: '#666666' },
+                        ' ',
+                        splitMultiline(level.examples),
+                        ' ',
+                        { text: `Level ${index + 1} Advanced`, fontSize: 10, color: '#666666' },
+                        ' ',
+                        splitMultiline(level.advSignals),
+                        ' ',
+                        { text: 'Examples', color: '#666666' },
+                        ' ',
+                        splitMultiline(level.advExamples),
+                    ]
+                );
+            });
+        }
+
+        var docDefinition = {
+            pageSize: 'A3',
+            pageOrientation: 'landscape',
+            pageMargins: 40,
+            fontSize: 10,
+            content: [
+                {
+                    layout: 'lightHorizontalLines',
+                    fontSize: 8,
+                    table: {
+                        headerRows: 1,
+                        dontBreakRows: true,
+                        tableCellPadding: 10,
+                        body: body
+                    }
+                }
+            ]
+        };
+        pdfMake.createPdf(docDefinition).download();
     }
 
     render() {
@@ -12,6 +80,7 @@ export default class Header extends React.Component {
                     color: #aaa;
                     font-size: 0.8em;
                     text-decoration: none;
+                    padding-left: 1em;
                 }
                 .download:hover {
                     text-decoration: underline;
@@ -26,7 +95,7 @@ export default class Header extends React.Component {
                                     <h1 className="h2 site-header__logo">Infinity Works</h1>
                                 </a>
                                 <div className="site-header__subtitle" style={{ color: 'white' }}>
-                                    Core Skills Radar&nbsp;
+                                    Core Skills Radar
                                     <a className="download" onClick={this.generatePdf} href="#">download</a>
                                 </div>
                             </div>
